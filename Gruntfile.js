c8a991895313b77a8df2d5a64e5d3631adf2074e
souvik <souvik@fusioncharts.com>
2015-08-25 15:02:22 +0530

    lint AB

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+       'use strict';
+       // Project configuration.
+       grunt.initConfig({
+               jasmine : {
+                       src : 'node_modules/grunt/src/*.js',
+                       options : {
+                               specs : 'node_modules/grunt/src/specs/*.js'
+                       }
+               },
+               jshint: {
+            myFiles: ['js/amitabh/*.js']
+        }
+       });
+       grunt.loadNpmTasks('grunt/node_modules/grunt-contrib-jasmine');
+       grunt.loadNpmTasks('grunt/node_modules/grunt-contrib-jshint');
+       grunt.registerTask('lint', ['jshint', 'jasmine']);
+};
